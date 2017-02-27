ebe5c98a3c7b8171576dcb882e7f9dfe1da0d92c
Julia Makrinova <julia.makrinova@gmail.com>
2017-02-27 21:03:42 +0300

    Added a feature of clients adding

---
+++ clientDB/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xceed.Wpf.Toolkit;
+
+namespace clientDB
+{
+    class Client
+    {
+        private string surname;
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string patronymic;
+
+        public string Patronymic
+        {
+            get { return patronymic; }
+            set { patronymic = value; }
+        }
+
+        private string number;
+
+        public string Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public Client(string surname, string name, string patronymic, string number)
+        {
+            this.surname = surname;
+            this.name = name;
+            this.patronymic = patronymic;
+            this.number = number;
+        }
+    }
+}
--- clientDB/MainWindow.xaml.cs
+++ clientDB/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace clientDB
 {
@@ -23,6 +24,84 @@ namespace clientDB
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        List<Client> clients = new List<Client>();
+
+        bool surnameEntered = false;
+        private void textBoxSurname_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (!surnameEntered)
+            {
+                textBoxSurname.Text = "";
+                textBoxSurname.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void textBoxSurname_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxSurname.Text))
+                surnameEntered = true;
+            else
+            {
+                textBoxSurname.Text = "Иванов";
+                surnameEntered = false;
+                textBoxSurname.Foreground = new SolidColorBrush(Colors.Gray);
+            }
+        }
+
+        bool nameEntered = false;
+        private void textBoxName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (!nameEntered)
+            {
+                textBoxName.Text = "";
+                textBoxName.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void textBoxName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxName.Text))
+                nameEntered = true;
+            else
+            {
+                textBoxName.Text = "Иван";
+                nameEntered = false;
+                textBoxName.Foreground = new SolidColorBrush(Colors.Gray);
+            }
+        }
+
+        bool patronymicEntered = false;
+        private void textBoxPatronymic_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (!patronymicEntered)
+            {
+                textBoxPatronymic.Text = "";
+                textBoxPatronymic.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void textBoxPatronymic_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxPatronymic.Text))
+                 patronymicEntered = true;
+            else
+            {
+                textBoxPatronymic.Text = "Иванович";
+                patronymicEntered = false;
+                textBoxPatronymic.Foreground = new SolidColorBrush(Colors.Gray);
+            }
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var client = new Client(textBoxSurname.Text, textBoxName.Text, textBoxPatronymic.Text, textBoxNumber.Text);
+            clients.Add(client);
+            textBoxSurname.Text = "";
+            textBoxName.Text = "";
+            textBoxPatronymic.Text = "";
+            textBoxNumber.Text = "";
         }
     }
 }
